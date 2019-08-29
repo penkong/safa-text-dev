8c8c5671b4c32a8b821feba00ee6d830a9ccb7e0
mk-nazemi <nazemi.works@gmail.com>
2019-08-29 08:57:06 +0430

    on road of config data and props

--- src/components/SafaQuasarText.vue
+++ src/components/SafaQuasarText.vue
@@ -1,12 +1,84 @@
 <template>
-  <q-input square outlined v-model="text" label="Square outlined" />
+  <div>
+    <q-input :id="id" square outlined v-model="text" label="Square outlined">
+      <template v-if="lang" v-slot:append>
+        <q-icon name="place" />
+      </template>
+      <template v-else v-slot:prepend>
+        <q-icon name="favorite" />
+      </template>
+      <!-- <template v-slot:hint>Field hint</template> -->
+    </q-input>
+  </div>
 </template>
 
 <script>
+import uid from "uuid/v4";
 export default {
-  // name: 'ComponentName',
+  name: "SafaQuasarText",
+  props: {
+    padding: {
+      type: Number,
+      default: 7
+    },
+    iconsize: { type: String, default: "24px" },
+    icon: { type: String, default: "none" },
+    color: {
+      type: String,
+      default: "white"
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    // rahnameee
+    helper: { type: String, default: "شما اینجایی" },
+    errorlabel: { type: String, default: "تصحیح شود" },
+    type: {
+      type: String,
+      default: "text",
+      required: true
+    },
+    m: {
+      // read write
+      type: String,
+      validator: v => ["r", "e", "ne"].includes(v),
+      default: "r"
+    },
+    c: {
+      // label
+      type: String,
+      validator: v =>
+        [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "auto"
+        ].includes(v),
+      default: "4"
+    },
+    value: { type: String },
+    label: { default: "", type: String }
+  },
   data() {
-    return {};
+    return {
+      lang: true,
+      idOfInput: null
+    };
+  },
+  computed() {
+    const generatedId = "S" + "_" + this.uid();
+
+    this.$set(this, "idOfInput", this.idOfInput);
   }
 };
 </script>
