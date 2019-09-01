87f97858bef5f07a00521bd63f2e84b0262234fe
mk-nazemi <nazemi.works@gmail.com>
2019-09-01 10:04:38 +0430

    load some sample for input comp

--- src/components/SafaDate.vue
+++ src/components/SafaDate.vue
@@ -1,57 +1,152 @@
 <template>
   <div class="safa-data">
-    <!-- label="Label" -->
-    <q-field
-      outlined
-      :hint="helper"
-      :value="text"
-      bottom-slots
-      stack-label
-      :dense="dense"
-      :rules="[
-        val => !!val  || errorlabel ,
-        val => val.length >= 3 || 'حداقل 3 کاراکتر'
-      ]"
-    >
-      <!-- <date-picker v-model="datetime" type="datetime" /> -->
-      <!--
-          display-format="dddd jDD jMMMM jYYYY"
-        format="jYYYY/jMM/jDD"
-      altFormat="YYYY-MM-DD HH:mm"-->
+    <q-field outlined :hint="helper" :value="text" bottom-slots stack-label :dense="dense">
       <date-picker
+        v-if="m == 'e'"
+        :editable="true"
         style="margin-top: 0.3rem;"
         :min="min"
         :max="max"
         :disabled="read"
-        :placeholder="label"
+        :placeholder="placeholder"
         @input="datetime=$event"
         color="teal"
         inputFormat="YYYY-MM-DD HH:mm"
+        format="jYYYY - jMM - jDD  --  HH:mm"
+        v-model="datetime"
+        type="datetime"
+        inputClass="form-control my-custom-class-name"
+        appendTo="body"
+        autoSubmit="false"
+      ></date-picker>
+      <date-picker
+        v-else
+        style="margin-top: 0.3rem;"
+        :min="min"
+        :max="max"
+        :disabled="read"
+        :placeholder="placeholder"
+        @input="datetime=$event"
+        color="teal"
+        inputFormat="YYYY-MM-DD HH:mm"
+        format="jYYYY - jMM - jDD  --  HH:mm"
         v-model="datetime"
         type="datetime"
         editable="false"
         inputClass="form-control my-custom-class-name"
         appendTo="body"
         autoSubmit="false"
-      >
-        <template v-slot:before>
-          <q-icon round name="event" dense flat class="cursor-pointer" icon="event" @click.stop></q-icon>
-        </template>
-      </date-picker>
-
-      <!-- <template v-slot:control>
-        <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
-      </template>-->
-
+      ></date-picker>
       <template v-slot:hint>Field hint</template>
-
-      <template v-slot:append>
-        <q-btn round dense flat icon="add" />
+      <template v-slot:after>
+        <q-item-label
+          style="margin-left: 1.5rem; font-family: 'behdad', 'Courier New', Courier, monospace;"
+          header
+        >{{ label }}</q-item-label>
+      </template>
+    </q-field>
+    <br />
+    <br />
+    <br />
+    <!-- //////////////////////////////////////////////////////////// -->
+    <q-btn
+      @click="show=true"
+      round
+      dense
+      type="button"
+      size="0.8rem"
+      :loading="loadingGear"
+      @click.prevent="simulateProgress"
+      color="light-blue-13"
+      text-color="white"
+      icon="today"
+    >
+      <template v-slot:loading>
+        <q-spinner-gears />
+      </template>
+    </q-btn>
+    <q-input
+      id="my-custom-editable-input"
+      type="text"
+      class="form-control is-editable"
+      placeholder="YYYY/MM/DD"
+      v-model="date"
+    />
+    <date-picker
+      element="my-custom-editable-input"
+      v-model="date"
+      format="jYYYY/jMM/jDD"
+      :editable="true"
+      :show="show"
+      @close="show=false"
+    />
+    <br />
+    <br />
+    <br />
+    <!-- //////////////////////////////////////////////////////////// -->
+    <q-field outlined :hint="helper" :value="text" bottom-slots stack-label :dense="dense">
+      <date-picker
+        v-if="m == 'e'"
+        :editable="true"
+        style="margin-top: 0.3rem;"
+        :min="min"
+        :max="max"
+        :disabled="read"
+        :placeholder="placeholder"
+        @input="datetime=$event"
+        color="teal"
+        inputFormat="YYYY-MM-DD HH:mm"
+        format="jYYYY - jMM - jDD  --  HH:mm"
+        v-model="datetime"
+        type="datetime"
+        inputClass="form-control my-custom-class-name"
+        appendTo="body"
+        autoSubmit="false"
+      ></date-picker>
+      <date-picker
+        v-else
+        style="margin-top: 0.3rem;"
+        :min="min"
+        :max="max"
+        :disabled="read"
+        :placeholder="placeholder"
+        @input="datetime=$event"
+        color="teal"
+        inputFormat="YYYY-MM-DD HH:mm"
+        format="jYYYY - jMM - jDD  --  HH:mm"
+        v-model="datetime"
+        type="datetime"
+        editable="false"
+        inputClass="form-control my-custom-class-name"
+        appendTo="body"
+        autoSubmit="false"
+      ></date-picker>
+      <template v-slot:hint>Field hint</template>
+      <template v-slot:after>
+        <q-item-label
+          style="margin-left: 1.5rem; font-family: 'behdad', 'Courier New', Courier, monospace;"
+          header
+        >{{ label }}</q-item-label>
       </template>
     </q-field>
   </div>
 </template>
-
+  <!-- <date-picker v-model="datetime" type="datetime" /> -->
+      <!--
+          display-format="dddd jDD jMMMM jYYYY"
+        format="jYYYY/jMM/jDD"
+        element="my-custom-input"
+      altFormat="YYYY-MM-DD HH:mm"-->
+        <!-- <q-btn >{{label}}</q-btn> -->
+        <!-- <template v-slot:before>
+          <q-icon round name="event" dense flat class="cursor-pointer" icon="event" @click.stop></q-icon>
+        </template>-->
+<!-- <template v-slot:before>
+          <q-icon round name="event" dense flat class="cursor-pointer" icon="event" @click.stop></q-icon>
+        </template>-->
+      <!-- <template v-slot:control>
+        <div class="self-center full-width no-outline" tabindex="0">{{text}}</div>
+      </template>-->
 <script>
 import VuePersianDatetimePicker from "vue-persian-datetime-picker";
 import uid from "uuid/v4";
@@ -62,8 +157,11 @@ export default {
   },
   data() {
     return {
+      date: "",
+      show: false,
       read: null,
       notEditable: null,
+      placeholder: "",
       text: "",
       loadingGear: false,
       idOfInput: null,
@@ -151,13 +249,15 @@ export default {
       setTimeout(() => {
         // we're done, we reset loading state
         this.loadingGear = false;
-      }, 1000);
+      }, 500);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import url("http://cdn.font-store.ir/behdad.css");
+
 .safa-data {
 }
 </style>
