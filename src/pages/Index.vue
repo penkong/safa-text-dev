ba6c220c155abf4ed5a1f0e972a55019e379e341
mk-nazemi <nazemi.works@gmail.com>
2019-09-14 17:00:51 +0430

    samll refactor

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,57 +1,47 @@
 <template>
-  <q-page style="width: 500px; margin: 0 auto;">
-    <div style="display: block;">
-      <SafaInput
-        :align="align"
-        :m="m"
-        :type="type"
-        :value="value"
-        :label="label"
-        :placeholder="placeholder"
-        :helper="helper"
-        :errorlabel="errorlabel"
-        :padding="padding"
-        :iconsize="iconsize"
-        :icon="icon"
-        :colored="color"
-        :c="c"
-        @inputer="checkVal"
-      />
-    </div>
+  <q-page class="flex flex-center">
+    <SafaDate
+      :align="align"
+      :m="m"
+      :type="type"
+      :value="value"
+      :label="label"
+      :icon="icon"
+      :helper="helper"
+      :errorlabel="errorlabel"
+      :c="c"
+      @inputer="logger"
+    ></SafaDate>
   </q-page>
 </template>
 
 <style></style>
 
 <script>
-import SafaInput from "../components/SafaInput/SafaInput";
+import SafaDate from "../components/SafaDate/SafaDate";
 export default {
   name: "PageIndex",
   components: {
-    SafaInput
+    SafaDate
   },
   data() {
     return {
       align: "right",
       m: "e",
+      minDate: "",
+      maxDate: "",
+      // for type date or datatime
       type: "text",
-      value: "",
-      label: "نام",
-      placeholder: "ورود اطلاعات",
+      value: "1398/01/01",
+      label: "زمان قرارداد",
+      icon: "today",
       helper: "شما اینجایی",
       errorlabel: " الزامی",
-      // not usefull
-      padding: 5,
-      // not usefull
-      iconsize: "24px",
-      // it must be base on quasar icon name
-      icon: "event",
-      color: "amber-1",
       c: "5"
     };
   },
   methods: {
-    checkVal(val) {
+    logger(val) {
       console.log(val);
     }
   }
